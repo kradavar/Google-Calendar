7d672866c6ecdd1d350b62fd904a233d62de51ec
Darya Krasava <kradavar@gmail.com>
2018-10-04 16:23:27 +0300

    Add chooseTableToCreate() function prototype

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 //import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      calendarToRender: "mounth",
+      mounthLength: 31
+    }
+  }
   render() {
     return (
       <div className="App">
--- src/Table-components/CalendarTable.js
+++ src/Table-components/CalendarTable.js
@@ -4,5 +4,35 @@ import Cell from './Cell';
 export default class CalendarTable extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      column: 7,
+      rows: (this.props.mounthLength / 7),
+    }
   }
+
+  chooseTableToCreate() {
+    switch (calendarToRender) {
+      case 'mounth': this.setState({
+        column: 7,
+        rows: (this.props.mounthLength / 7)
+      });
+        break;
+      case 'week': this.setState({
+        column: 7,
+        rows: 1
+      });
+        break;
+      case 'day': this.setState({
+        column: 1,
+        rows: 1
+      });
+        break;
+      default: break;
+    }
+  }
+
+  render() {
+
+  }
+
 }
