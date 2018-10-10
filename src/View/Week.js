9453b4983d5c5d60ea8480b4004e5588631ad530
Darya Krasava <kradavar@gmail.com>
2018-10-10 12:29:44 +0300

    new Week view

---
+++ src/View/Week.js
@@ -0,0 +1,32 @@
+import Day from "./Day";
+import moment from "moment";
+
+export default function Week(props) {
+  const renderWeek = () => {
+    const date = moment(this.props.renderedDate);
+    const wholeWeekStart = date.clone().startOf("isoWeek");
+    const wholeWeekEnd = date.clone().endOf("isoWeek");
+    const week = [];
+    for (
+      let currDate = wholeWeekStart.clone();
+      currDate <= wholeWeekEnd;
+      currDate.add(1, "day")
+    ) {
+      week.push(<Day view={props.view} date={props.renderedDate} />);
+    }
+
+    return week;
+  };
+
+  const week = renderWeek();
+  return (
+    <div>
+      {props.view === "week" ? (
+        { week }
+      ) : (
+        <Day renderedDate={props.renderedDate} view={props.view} />
+      )}
+      )}
+    </div>
+  );
+}
