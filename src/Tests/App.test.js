cde698d4444f8f4209af4c5a6817f0c3f3485013
Darya Krasava <kradavar@gmail.com>
2018-10-18 16:39:52 +0300

    Add some app tasts

--- src/Tests/App.test.js
+++ src/Tests/App.test.js
@@ -2,8 +2,20 @@ import React from "react";
 
 import { shallow } from "enzyme";
 import ViewTypeSwitcher from "../View/Switchers/ViewTypeSwitcher";
+import App from "../App";
+import CalendarTable from "../View/Table-components/CalendarTable";
 
 it("renders three buttons for changing type of view ", () => {
-  const wrapper = shallow(<ViewTypeSwitcher view="month" />);
+  const wrapper = shallow(<ViewTypeSwitcher />);
   expect(wrapper.find("div").children().length).toEqual(3);
+});
+
+it("renders name of rendered month", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find(".current-month").text()).toEqual("October");
+});
+
+it("rendering of month class", () => {
+  const wrapper = shallow(<CalendarTable view="month" />);
+  expect(wrapper.find(".month").exists());
 });
