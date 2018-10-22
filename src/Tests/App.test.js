74b3f05c22f56f41d877db70d2f4ef4222f5cfe3
Darya Krasava <kradavar@gmail.com>
2018-10-22 14:47:04 +0300

    Add some small tests for App

--- src/Tests/App.test.js
+++ src/Tests/App.test.js
@@ -4,10 +4,10 @@ import { shallow } from "enzyme";
 import ViewTypeSwitcher from "../View/Switchers/ViewTypeSwitcher";
 import App from "../App";
 import CalendarTable from "../View/Table-components/CalendarTable";
-
+import Button from "../View/Switchers/Button";
 it("renders three buttons for changing type of view ", () => {
   const wrapper = shallow(<ViewTypeSwitcher />);
-  expect(wrapper.find("div").children().length).toEqual(3);
+  expect(wrapper.find(Button).length).toEqual(3);
 });
 
 it("renders name of rendered month", () => {
@@ -18,4 +18,26 @@ it("renders name of rendered month", () => {
 it("rendering of month class", () => {
   const wrapper = shallow(<CalendarTable view="month" />);
   expect(wrapper.find(".month").exists());
+});
+
+it("changes type of view on click", () => {
+  const wrapper = shallow(<ViewTypeSwitcher view="month" />);
+  expect(
+    wrapper
+      .find(Button)
+      .at(1)
+      .props().value
+  ).toEqual("week");
+
+  wrapper
+    .find(Button)
+    .at(1)
+    .simulate("click");
+
+  console.log(
+    wrapper
+      .find(Button)
+      .at(1)
+      .props().view
+  );
 });
