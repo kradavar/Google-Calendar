d4f5a49a7700ec93ed1b86925b41f8dd403ce5ed
Darya Krasava <kradavar@gmail.com>
2018-10-22 14:16:01 +0300

    Add modal test

---
+++ src/Tests/Modal.test.js
@@ -0,0 +1,10 @@
+import { shallow } from "enzyme";
+import React from "react";
+
+import Day from "../View/Table-components/Day";
+import moment from "moment";
+
+it("renders a modal window", () => {
+  const wrapper = shallow(<Day renderedDate={moment()} view="month" />);
+  expect(wrapper.find(".modal").exists());
+});
