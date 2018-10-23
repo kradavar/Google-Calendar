f4ae94a96dccbb708012783f4b7732810892b574
Darya Krasava <kradavar@gmail.com>
2018-10-23 17:25:35 +0300

    Add Date formats const

---
+++ src/Model/DateFormats.js
@@ -0,0 +1,6 @@
+export const DATE_FORMATS = {
+  HOUR: "HH",
+  TIME: "HH:mm",
+  DATE: "YYYY-MM-DD",
+  DATE_WITH_TIME: "YYYY-MM-DD HH:mm"
+};
--- src/Model/getRenderedDateInfo.js
+++ src/Model/getRenderedDateInfo.js
@@ -1,24 +1,8 @@
 import moment from "moment";
 
-export const formatDateWithTime = date =>
-  moment(date).format("YYYY-MM-DD HH:mm");
-export const formatTime = date => moment(date).format("HH:mm");
-export const formatDate = date => moment(date).format("YYYY-MM-DD");
+export const formatDate = (date, format) => moment(date).format(format);
 
-export const getValueOfMoment = (renderedDate, type) => {
-  switch (type) {
-    case "time":
-      return formatTime(renderedDate);
-    case "date":
-      return formatDate(renderedDate);
-    case "min":
-      return moment(renderedDate).minute();
-    case "day-start":
-      return moment().startOf("day");
-    default:
-      return formatDateWithTime(renderedDate);
-  }
-};
+export const createMoment = dateStr => moment(dateStr);
 
 export const getDuration = (start, end, type) =>
   moment(end).diff(moment(start), type);
