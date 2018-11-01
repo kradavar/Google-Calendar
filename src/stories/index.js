91c511403298d9220ed05068eccc501790df34bd
Darya Krasava <kradavar@gmail.com>
2018-11-01 17:50:49 +0300

    start storybook

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { linkTo } from "@storybook/addon-links";
 
-import { Button, Welcome } from '@storybook/react/demo';
+import { Welcome } from "@storybook/react/demo";
+import { Button } from "../View/Switchers/Button";
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf("Welcome", module).add("to Storybook", () => (
+  <Welcome showApp={linkTo("Button")} />
+));
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
+storiesOf("Button", module)
+  .add("with text", () => (
+    <Button onClick={action("clicked")}>My Button</Button>
+  ))
+  .add("validation", () => (
+    <React.Fragment>
+      {["error", "warn", "valid", null].map(valid => (
+        <Button key={valid} size="100%" valid={valid}>
+          click this button
+        </Button>
+      ))}
+    </React.Fragment>
+  ))
+  .add("with some emoji", () => (
+    <Button onClick={action("clicked")}>
       <span role="img" aria-label="so cool">
         😀 😎 👍 💯
       </span>
