3ef72b26b6869ed15c0d1d7ed938357e6b47daed
Darya Krasava <kradavar@gmail.com>
2018-11-01 09:29:19 +0300

    delete DayWeekHeader from Week

--- src/View/Table-components/Week.js
+++ src/View/Table-components/Week.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Day from "./Day";
 import "../../Styles/Cell.css";
-import { DayWeekHeader } from "./DayWeekHeader";
 import { DATE_FORMATS } from "../../Model/DateFormats";
 import { formatDate } from "../../Model/getRenderedDateInfo";
 import { SharedViewContext } from "../../Context";
@@ -11,20 +10,12 @@ export const Week = ({ renderedDate, view }) => {
     const wholeWeekStart = renderedDate.clone().startOf("isoWeek");
     const wholeWeekEnd = renderedDate.clone().endOf("isoWeek");
     const days = [];
-    const daysHeader = [];
     for (
       let currDate = wholeWeekStart.clone();
       currDate <= wholeWeekEnd;
       currDate.add(1, "day")
     ) {
       const date = currDate.clone();
-      daysHeader.push(
-        <DayWeekHeader
-          renderedDate={formatDate(date, DATE_FORMATS.DATE_OF_MONTH)}
-          day={formatDate(date, DATE_FORMATS.DAY)}
-          key={formatDate(date, DATE_FORMATS.DATE_WITH_TIME)}
-        />
-      );
       days.push(
         <Day
           view={view}
