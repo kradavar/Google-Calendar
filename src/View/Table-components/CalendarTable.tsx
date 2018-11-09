e872e9fe14cef2d9b7f5d6f2b5900ee10f5970a8
Darya Krasava <kradavar@gmail.com>
2018-11-09 16:45:31 +0300

    change monthDate type to string

--- src/View/Table-components/CalendarTable.tsx
+++ src/View/Table-components/CalendarTable.tsx
@@ -5,6 +5,8 @@ import { Week } from "./Week";
 import { SharedViewContext } from "../../Context";
 import * as moment from "moment";
 import { VIEW } from "../../constants/ViewTypes";
+import { DATE_FORMATS } from "../../constants/DateFormats";
+import { formatDate } from "../../Model/getRenderedDateInfo";
 
 export const CalendarTable: React.SFC<{}> = ({}) => {
   return (
@@ -18,7 +20,9 @@ export const CalendarTable: React.SFC<{}> = ({}) => {
       }) => (
         <div className="calendar">
           {view === VIEW.MONTH ? (
-            <Month monthDate={renderedDate} />
+            <Month
+              monthDate={formatDate(renderedDate, DATE_FORMATS.DATE_WITH_TIME)}
+            />
           ) : (
             <Week weekDate={renderedDate} />
           )}
--- src/View/Table-components/Month.tsx
+++ src/View/Table-components/Month.tsx
@@ -4,29 +4,29 @@ import "../../Styles/Cell.css";
 import { MonthHeader } from "./MonthHeader";
 import * as moment from "moment";
 
-export const Month: React.SFC<{ monthDate: moment.Moment }> = ({
-  monthDate
-}) => {
-  const getWeekCount = (date: moment.Moment): number => {
+export const Month: React.SFC<{ monthDate: string }> = ({ monthDate }) => {
+  const getWeekCount = (date: string): number => {
+    const cloneDate = moment(date);
     // week number of 1st day of the month
-    const start: number = date.date(1).isoWeek();
+    const start: number = cloneDate.date(1).isoWeek();
     // week number of the last day of the month
-    const end: number = date.date(date.daysInMonth()).isoWeek();
+    const end: number = cloneDate.date(cloneDate.daysInMonth()).isoWeek();
     const duration: number = end - start + 1;
     if (duration < 0) {
-      return date.isoWeeksInYear() + duration;
+      return cloneDate.isoWeeksInYear() + duration;
     }
     return duration;
   };
 
   const renderMonth = () => {
-    const currentDate: moment.Moment = monthDate.clone().startOf("month");
+    const currentDate: moment.Moment = moment(monthDate).startOf("month");
     const weeks: Array<JSX.Element> = [];
-    const duration: number = getWeekCount(monthDate.clone());
+    const duration: number = getWeekCount(monthDate);
 
     for (let i = 0; i < duration; i++) {
-      weeks.push(<Week weekDate={currentDate.clone()} key={i} />);
-      currentDate.add(1, "week");
+      weeks.push(
+        <Week weekDate={currentDate.clone().add(i, "week")} key={i} />
+      );
     }
 
     return weeks;
