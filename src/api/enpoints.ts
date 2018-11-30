ed19235223f809e23f1e8e676e6e220658baea10
Darya Krasava <kradavar@gmail.com>
2018-11-30 14:46:50 +0300

    add endpoints constants

--- src/api/enpoints.ts
+++ src/api/enpoints.ts
@@ -1,7 +1,6 @@
+const BASE_API_URL = "http://localhost:5000";
 
-// Here should be all endpoints
-
-// TODO add versions
-const BASE_API_URL = 'http://localhost:5000';
-
-const USER_SIGN_IN = `${BASE_API_URL}/signin`;
+export const USER_SIGN_IN = `${BASE_API_URL}/signin`;
+export const USER_SIGN_UP = `${BASE_API_URL}/signup`;
+export const USER_SIGN_OUT = `${BASE_API_URL}/signout`;
+export const EVENTS = `${BASE_API_URL}/events`;
