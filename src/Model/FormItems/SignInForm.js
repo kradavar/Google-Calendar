725f7628e8ff26b2052dce4cfdecb212c01b8b66
Darya Krasava <kradavar@gmail.com>
2018-12-03 16:38:50 +0300

    add basics for toasts

--- src/Model/FormItems/SignInForm.js
+++ src/Model/FormItems/SignInForm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { reduxForm, SubmissionError } from "redux-form";
+import { ToastContainer, ToastStore } from "react-toasts";
 
 import { FormInputWithLabel } from "./FormInputWithLabel";
 import { Button } from "../../View/Switchers/Button";
@@ -11,9 +12,8 @@ const SignInFormComponent = ({ handleSubmit, reset, signIn, handleClose }) => {
   const submit = values =>
     signIn(values)
       .then(() => {
-        // add notification like toast.js or something?
-        //  YES :)
-        console.log("MODAL IS CLOSING,BYE");
+        debugger;
+        ToastStore.success("Welcome!");
         return handleClose();
       })
       .catch(error => {
@@ -24,24 +24,33 @@ const SignInFormComponent = ({ handleSubmit, reset, signIn, handleClose }) => {
       });
 
   return (
-    <form onSubmit={handleSubmit(submit)}>
-      <FormInputWithLabel name="username" label="Your username: " type="text" />
-      <FormInputWithLabel
-        name="password"
-        label="Your password: "
-        type="password"
-      />
-      <div className="modal-footer">
-        <Button
-          classes="btn-outline-secondary"
-          data-dismiss="modal"
-          onClick={reset}
-          value="Clear"
+    <React.Fragment>
+      <form onSubmit={handleSubmit(submit)}>
+        <FormInputWithLabel
+          name="username"
+          label="Your username: "
+          type="text"
         />
-
-        <Button classes="btn-outline-success" value={SIGN.IN} type="submit" />
-      </div>
-    </form>
+        <FormInputWithLabel
+          name="password"
+          label="Your password: "
+          type="password"
+        />
+        <div className="modal-footer">
+          <Button
+            classes="btn-outline-secondary"
+            data-dismiss="modal"
+            onClick={reset}
+            value="Clear"
+          />
+          <Button classes="btn-outline-success" value={SIGN.IN} type="submit" />
+        </div>
+      </form>
+      <ToastContainer
+        store={ToastStore}
+        position={ToastContainer.POSITION.TOP_RIGHT}
+      />
+    </React.Fragment>
   );
 };
 
